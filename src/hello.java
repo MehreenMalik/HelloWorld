411c8887e09818b9d7efe8077664a4d1b3814a4e
MehreenMalik <mehreenmalik08@gmail.com>
2018-04-16 13:16:30 +0100

    Prints Hello World

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author Mehreen Malik
+ * mehreenmalik08@gmail.com
+ * Prints Hello World
+ */
+
+public class hello 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+	}
+}
